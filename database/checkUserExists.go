a2feb92ab52ea6409c278547c7283a73fe7ce869
Hernandez <lu97is@gmail.com>
2021-02-17 22:51:48 -0700

    added missing env variable

--- database/checkUserExists.go
+++ database/checkUserExists.go
@@ -4,16 +4,18 @@ import (
 	"context"
 	"time"
 
+	"github.com/lu97chi/twittor/config"
 	"github.com/lu97chi/twittor/models"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 // CheckUserExists function to see if user exits
 func CheckUserExists(email string) (models.User, bool, string) {
+	conf := config.New()
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
-	db := MongoCN().Database("twittor")
+	db := MongoCN().Database(conf.MongoDB.Database)
 	col := db.Collection(models.DBCollection)
 
 	condition := bson.M{"email": email}
